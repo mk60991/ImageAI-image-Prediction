e2eb657c953d87e44111ad1dc9ab54a567bc8966
Manish Kumar <mk60991@gmail.com>
2018-06-25 23:06:59 +0530

    Add files via upload

---
+++ Multiple-Image-Predict/multipleImagesPredict.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 07:27:51 2018
+
+@author: hp
+"""
+
+from imageai.Prediction import ImagePrediction
+import os
+
+execution_path = os.getcwd()
+
+
+multiple_prediction = ImagePrediction()
+multiple_prediction.setModelTypeAsResNet()
+multiple_prediction.setModelPath(os.path.join(execution_path, "C:\\Users\\hp\\Downloads\\object Detection3\\resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+multiple_prediction.loadModel()
+
+
+all_images_array = []
+
+
+all_files = os.listdir(execution_path)
+for each_file in all_files:
+    
+    
+    if(each_file.endswith(".jpg") or each_file.endswith(".png")):
+        
+        all_images_array.append(each_file)
+
+
+results_array = multiple_prediction.predictMultipleImages(all_images_array, result_count_per_image=5)
+
+
+for each_result in results_array:
+    predictions, percentage_probabilities = each_result["predictions"], each_result["percentage_probabilities"]
+    for index in range(len(predictions)):
+        print(predictions[index] + " : " + percentage_probabilities[index])
+    print("-----------------------")
+
